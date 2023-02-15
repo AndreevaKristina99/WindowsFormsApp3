cfb529489b44e2faad5b7ffdf73a6e04cb5029f6
Кристина Андреева <kandreeva952@gmail.com>
2023-02-16 00:38:07 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/Form2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DrawBarcode(textBox1.Text);
+        }
+        private void DrawBarcode(string code, int resolution = 20) // resolution - пикселей на миллиметр
+        {
+            int numberCount = 25; // количество цифр
+            float height = 25.93f * resolution; // высота штрих кода
+            float lineHeight = 22.85f * resolution; // высота штриха
+            float leftOffset = 3.63f * resolution; // свободная зона слева
+            float rightOffset = 2.31f * resolution; // свободная зона справа
+                                                    //штрихи, которые образуют правый и левый ограничивающие знаки,
+                                                    //а также центральный ограничивающий знак должны быть удлинены вниз на 1,65мм
+            float longLineHeight = lineHeight + 1.65f * resolution;
+            float fontHeight = 2.75f * resolution; // высота цифр
+            float lineToFontOffset = 0.165f * resolution; // минимальный размер от верхнего края цифр до нижнего края штрихов
+            float lineWidthDelta = 0.15f * resolution; // ширина 0.15*{цифра}
+            float lineWidthFull = 1.35f * resolution; // ширина белой полоски при 0 или 0.15*9
+            float lineOffset = 0.2f * resolution; // между штрихами должно быть расстояние в 0.2мм
+
+            float width = leftOffset + rightOffset + 6 * (lineWidthDelta + lineOffset) + numberCount * (lineWidthFull + lineOffset); // ширина штрих-кода
+
+            Bitmap bitmap = new Bitmap((int)width, (int)height); // создание картинки нужных размеров
+            Graphics g = Graphics.FromImage(bitmap); // создание графики
+
+            Font font = new Font("Arial", fontHeight, FontStyle.Regular, GraphicsUnit.Pixel); // создание шрифта
+
+            StringFormat fontFormat = new StringFormat(); // Центрирование текста
+            fontFormat.Alignment = StringAlignment.Center;
+            fontFormat.LineAlignment = StringAlignment.Center;
+
+            float x = leftOffset; // позиция рисования по x
+            for (int i = 0; i < numberCount; i++)
+            {
+                int number = Convert.ToInt32(code[i].ToString()); // число из кода
+                if (number != 0)
+                {
+                    g.FillRectangle(Brushes.Black, x, 0, number * lineWidthDelta, lineHeight); // рисуем штрих
+                }
+                RectangleF fontRect = new RectangleF(x, lineHeight + lineToFontOffset, lineWidthFull, fontHeight); // рамки для буквы
+                g.DrawString(code[i].ToString(), font, Brushes.Black, fontRect, fontFormat); // рисуем букву
+                x += lineWidthFull + lineOffset; // смещаем позицию рисования по x
+
+                if (i == 0 || i == numberCount / 2 || i == numberCount - 1) // если это начало, середина или конец кода рисуем разделители
+                {
+                    for (int j = 0; j < 2; j++) // рисуем 2 линии разделителя
+                    {
+                        g.FillRectangle(Brushes.Black, x, 0, lineWidthDelta, longLineHeight); // рисуем длинный штрих
+                        x += lineWidthDelta + lineOffset; // смещаем позицию рисования по x
+                    }
+                }
+            }
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // делаем чтобы картинка помещалась в pictureBox
+            pictureBox1.Image = bitmap; // устанавливаем картинку
+        }
+
+
+    }
+}
